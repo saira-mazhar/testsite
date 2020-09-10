98dce029418fb095158266aa013c8fd771601045
saira-mazhar <70911705+saira-mazhar@users.noreply.github.com>
2020-09-10 22:00:17 +0100

    Add files via upload

---
+++ resp1.js
@@ -0,0 +1,13 @@
+
+boxmain= document.querySelector('.box-main')
+firsthalf= document.querySelector('.firsthalf')
+secondhalf= document.querySelector('.secondhalf')
+
+
+secondhalf.addEventListener('click',()=>{
+    box-main.classList.toggle('v-class-resp');
+    firsthalf.classList.toggle('v-class-resp');
+    
+
+
+})
